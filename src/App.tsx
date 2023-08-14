d72437d8c64dd3b493cab238c00ed2680d6124c0
ngyngcphu <nguyenngocphu225@gmail.com>
2023-08-14 17:13:37 +0700

    feat(code-quality-toolset): setup eslint, prettier, lint-staged and husky.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
 function App() {
-  return (
-    <h1 className="text-3xl font-bold underline text-red-500">
-      Hello world!
-    </h1>
-  );
+  return <h1 className='text-3xl font-bold underline text-red-500'>Hello world!</h1>;
 }
 
 export default App;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,4 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
